1df0c47dff0e6e17831b757d4c94755bab04b293
Eduardo Eller Behr <eduardo.behr@hotmail.com>
2021-09-24 21:06:25 -0300

    Improved results of PI)controller_designer

--- PI_controller_designer.py
+++ PI_controller_designer.py
@@ -1,14 +1,15 @@
 #! python3
 import control as ct
-from numpy import pi, tan, angle, abs, log10, sqrt
+from numpy import pi, tan, angle, abs, log10, sqrt, logspace
 from matplotlib import pyplot as plt
 s=ct.tf([1,0], [1])
 G=10/(1+s)
+print(f"FTMA(s) = {G}")
 
 mfnc = ct.margin(G)[1]
 wcnc = ct.margin(G)[3]
 
-print(f'{mfnc = :.3}; {wcnc = :.3}')
+print(f'{mfnc = :.3}°; {wcnc = :.3} rad/s')
 
 wcc = 1 # frequência de corte desejada (compensada)
 mfc = 60 # margem de fase desejada (em graus)
@@ -34,22 +35,24 @@ wz = abs(wcc/tan((-mfc + angGc_deg -90)*pi/180))
 Kp = wcc/(sqrt(wcc**2+wz**2)*absGc)
 
 print(f"{angGc_deg =  :.3}°, {absGc = :.4} ({magGc_dB:.4} dB)")
-print(f"{wz = :.4}, {Kp = :.4}")
+print(f"{wz = :.4} rad/s, {Kp = :.4}")
 
 C = Kp*(s+wz)/s
-
+print(f"C(s) = {C}")
 CG = C*G
+print(f"FTMF(s) = {C}")
 
 mfc_final = ct.margin(CG)[1]
 wc_final = ct.margin(CG)[3]
-print(f'{mfc_final = :.4}; {wc_final = :.4}')
+print(f'{mfc_final = :.4}°; {wc_final = :.4} rad/s')
 
 if 1:
 	central_freq = sqrt(wcnc*wcc)
-	
-	
-	ct.bode_plot(G, dB=True)
-	ct.bode_plot(C, dB=True)
-	ct.bode_plot(CG, dB=True)
+	maxfreq_exp = (int(log10(max([wcnc,wcc])*10))+1)
+	minfreq_exp = (int(log10(min([wcnc,wcc])*10))-2)
+	omega = logspace(maxfreq_exp, minfreq_exp, 1000)
+	ct.bode_plot(G, dB=True, omega=omega)
+	ct.bode_plot(C, dB=True, omega=omega)
+	ct.bode_plot(CG, dB=True, omega=omega)
 	plt.legend(["G", "C", "CG"])
 	plt.show()
